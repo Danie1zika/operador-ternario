298207c70e17c7347c3a1eb0aa9398f6c1e68593
Danie1zika <danielbrandao03420@gmail.com>
2024-04-17 11:26:31 -0400

    questao12 enviado

---
+++ questao12.js
@@ -0,0 +1,11 @@
+/**Questão 12: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa pode votar. A variável idade contém a idade da 
+pessoa. Atribua o resultado à variável podeVotar.  */
+
+let idade = 20;
+let podeVotar = idade >= 16 ? 'Pode votar' : 'Não pode votar';
+
+console.log(`A pessoa ${podeVotar}.`);
+
+
+
